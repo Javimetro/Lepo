bf9799e62a632f645d68a1dee369233ecc32a686
javimetro <javierj@metropolia.fi>
2024-03-20 12:59:31 +0200

    seting up

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, 'index.html'),
-        login: path.resolve(__dirname, 'src/login.js'),
-        menu: path.resolve(__dirname, 'src/menu.js'),
-        modifyCredentials: path.resolve(__dirname, 'src/menu.js'),
-        register: path.resolve(__dirname, 'src/register.js'),// Adding login.js explicitly
+        main: path.resolve(__dirname, 'src/index.html'),
+        login: path.resolve(__dirname, 'src/login.html'),
+        menu: path.resolve(__dirname, 'src/menu.html'),
+        modifyCredentials: path.resolve(__dirname, 'src/modifyCredentials.html'),
+        register: path.resolve(__dirname, 'src/register.html'),// Adding login.js explicitly
         // Add other entry points if necessary
       }
     }
